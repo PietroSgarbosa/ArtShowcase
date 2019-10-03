70a7f0473ca80cca324fc998ff6984f68f84d1bd
Pietro Sgarbosa <pietrosgarbosa@gmail.com>
2019-10-03 13:15:08 -0300

    'MSG'

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ let conn = require('../dbconfig');
 
 module.exports = (app)=>{
 
-
     app.get('/user/user_list', (req, res)=>{
 
         conn.query("SELECT * FROM project_artshowcase.cadastro", (err, results)=>{
@@ -20,60 +19,69 @@ module.exports = (app)=>{
     app.post('/user/register', (req, res)=>{
             
         let post  = req.body;
-        let user = post.usuario;
+        let user = post.nick;
         let nick = post.nick;
         let mail = post.email;
         let pw = post.senha;
         let age = post.idade;
         let gender = post.sexo;
 
-        // insert statment
+        // insert statment (TESTING PURPOSE)
         let sql = `INSERT INTO project_artshowcase.cadastro
-        (NOME_USUAR,
-        NICK_USUAR,
-        MAIL_USUAR,
-        SENH_USUAR,
-        IDAD_USUAR,
-        SEXO_USUAR)
-        VALUES
-        ('`+user+`',
-        '`+nick+`',
-        '`+mail+`',
-        '`+pw+`',
-        `+age+`,
-        '`+gender+`');`;
+                    (NOME_USUAR, NICK_USUAR, MAIL_USUAR, SENH_USUAR, IDAD_USUAR, SEXO_USUAR)
+                     VALUES ('teste', 'teste', 'teste@teste', '123testando', '22', 'masculino');`;
+
+        
         
         // execute the insert statment
         conn.query(sql, (err, results)=>{
 
             if(err){
                 res.send(err);
+                
             }else{
                 res.send(results);
+                app.render('Registro.vue', function (err, html) {
+                    // ...
+                  })
+                
             }
         });
-
-        conn.end();
         
     });
+    /*NOT WORKING YET
+        //TEST INSERT
+            /*let sql = `INSERT INTO project_artshowcase.cadastro
+                (NOME_USUAR,
+                NICK_USUAR,
+                MAIL_USUAR,
+                SENH_USUAR,
+                IDAD_USUAR,
+                SEXO_USUAR)
+                VALUES
+                ('`+user+`',
+                '`+nick+`',
+                '`+mail+`',
+                '`+pw+`',
+                `+age+`,
+                '`+gender+`');`;*/
 
-    //TEST INSERT
-    /*app.get('/user/register', (req, res)=>{
-        let query = `INSERT INTO project_artshowcase.cadastro
-                    (NOME_USUAR, NICK_USUAR, MAIL_USUAR, SENH_USUAR, IDAD_USUAR, SEXO_USUAR)
-                     VALUES ('teste', 'teste', 'teste@teste', '123testando', '22', 'masculino');`;
+            /*app.get('/user/register', (req, res)=>{
+                let query = `INSERT INTO project_artshowcase.cadastro
+                            (NOME_USUAR, NICK_USUAR, MAIL_USUAR, SENH_USUAR, IDAD_USUAR, SEXO_USUAR)
+                            VALUES ('teste', 'teste', 'teste@teste', '123testando', '22', 'masculino');`;
 
-        conn.query(query, (err, results)=>{
+                conn.query(query, (err, results)=>{
 
-            if(err){
-                res.send(err);
-            }else{
-                res.send(results);
-            }
-        });
-    });*/
+                    if(err){
+                        res.send(err);
+                    }else{
+                        res.send(results);
+                    }
+                });
+            });
+    */
    
 }
-    //AINDA NÃO FUNCIONAL
-     /**/
+    
 
