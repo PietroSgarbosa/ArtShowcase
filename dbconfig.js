b490fdb9bad2496395b4d3b88741530632dda3a0
Rafael Carvalho <az.carta@gmail.com.com>
2019-10-15 07:06:52 -0300

    jwt added

--- dbconfig.js
+++ dbconfig.js
@@ -1,11 +1,14 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 let connection  = mysql.createConnection({
-  host    : '127.0.0.1',
-  port    : '3000',
-  user    : 'user_test',
-  password: '123456',
-  database: 'project_artshowcase'
+  host    : process.env.DB_HOST,
+  port    : process.env.DB_PORT,
+  user    : process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA
 });
 
 connection.connect((err) => {
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const consign = require('consign');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 let port = 3035;
 let ip = '127.0.0.1';
 let app = express();
@@ -10,6 +11,7 @@ let app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
 
 consign().include('routes').into(app);
 
