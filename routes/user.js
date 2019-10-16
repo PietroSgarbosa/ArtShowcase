d19cf6895438ebbf23e148ebd7ac81c48e3a08d8
Sky-Horse-Labs <sky.horse.development@gmail.com>
2019-10-16 07:25:56 -0300

    Workin changes
    login, register, jwt

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,53 @@
-let conn = require('../dbconfig');
+const conn = require('../dbconfig');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'secret';
+
 
 module.exports = (app)=>{
-
+   
     app.post('/user/login', (req, res)=>{
-
+        
         const {
-            nick,
+            usuario,
             senha
         } = req.body;
 
-        let sql = `SELECT CODI_USUAR FROM project_artshowcase.cadastro WHERE NICK_USUAR = '${nick}' AND SENH_USUAR = '${senha}'`;
+        let sql = `SELECT CODI_USUAR, NOME_USUAR FROM cadastro WHERE NICK_USUAR = '${usuario}' AND SENH_USUAR = '${senha}'`;
 
         conn.query(sql, (err, results)=>{
-                       
+                  
             if(err){
                 console.log(err);
                 res.sendStatus(400);
             }else if (results.length != 1){
                 console.log("Usuário ou senha incorreto(s)");
-                res.sendStatus(403); //FORBIDDEN
+                res.sendStatus(403);
             }else{
-                console.log("Login realizado!");
-                res.send({ cod_user: user_cod, 
-                           status : 200});
-            }
+                const user={
+                    id : results[0].CODI_USUAR,
+                    name : results[0].NOME_USUAR
+                };
+                jwt.sign({user}, JWT_SECRET,(err, token) =>{//HERE'S WHERE ALL THE TOKEN SHENANIGANS BEGINS. 
+                    res.status(200).json({token});          //ALL THE INFORMATION CONTAINED IN THE OBJECT "user"
+                });                                         //WILL BE ENCODED INTO A TOKEN, AND THE WORD 'secret'
+                                                            //IS THE KEY TO DECODE IT. THE LOGGED USER WILL HAVE A SPECIFIC TOKEN
+            }                                               //AND A TIMESTAMP FOR US TO KEEP TRACK OF HIM/HER/IT
         });
 
+    });
+
+    app.post('/user/upload_image',verifyToken, (req, res) =>{
+        
+        jwt.verify(req.token, JWT_SECRET, (err, authData)=>{//HERE THE API RECIEVES THE TOKEN AND THEN DECODE IT, 
+            if(err){                                        //TURNING IT INTO A JSON THAT CONTAINS ALL THE DATA FROM THE USER.
+                res.sendStatus(403);                        //ALSO, HERE'S WHERE WE PUT OUR LITTLE SECRET FOR DECONDING.
+            }else{                                          //THERE ARE MUCH BETTER WAYS TO HIDE THESE HARD CODED PASSWORDS,
+                res.json({                                  //I'LL BE LOOKING FOWARD ON USING THOSE WAYS...
+                    message : 'post created',
+                    authData
+                });
+            }
+        });
     });
 
     app.post('/user/register', (req, res)=>{
@@ -40,12 +62,12 @@ module.exports = (app)=>{
         } = req.body;
 
         // insert statment (TESTING PURPOSE)
-        let sql = `INSERT INTO project_artshowcase.cadastro
+        let sql = `INSERT INTO cadastro
                     (NOME_USUAR, NICK_USUAR, MAIL_USUAR, SENH_USUAR, IDAD_USUAR, SEXO_USUAR)
                      VALUES ('${usuario}', '${nick}', '${email}', '${senha}', '${idade}', '${sexo}');`;
 
         // execute the insert statment
-        conn.query(sql, (err, results)=>{
+        conn.query(sql, (err)=>{
 
             if(err){
                 console.log(err);
@@ -57,21 +79,29 @@ module.exports = (app)=>{
         
     });
 
-    //TEST INSERT
-    /*app.get('/user/register', (req, res)=>{
-        let query = `INSERT INTO project_artshowcase.cadastro
-                    (NOME_USUAR, NICK_USUAR, MAIL_USUAR, SENH_USUAR, IDAD_USUAR, SEXO_USUAR)
-                     VALUES ('teste', 'teste', 'teste@teste', '123testando', '22', 'masculino');`;
+    
+    // TOKEN OUTPUT (WHAT IT RETURNS) --> Authorization: Bearer <access_token> //Bearer moves a token to the Header of the request
 
-        conn.query(query, (err, results)=>{
+  //VERIFY TOKEN
+    function verifyToken (req, res, next){
+        //GET AUTH HEADER VALUE
+        const bearerHeader = req.headers['authorization'];
+        //CHECK IF bearerHeader IS UNDEFINED
+        if(typeof bearerHeader !== 'undefined'){
+            //SPLIT AT SPACE
+            const bearer = bearerHeader.split(' '); //TURNS STRING INTO AN ARRAY --> [Bearer, <access_token>]
+            //GET TOKEN FROM ARRAY -->[Bearer, <access_token> <--GET THIS GUY]
+            const bearerToken = bearer[1];
+            //SET THE TOKEN
+            req.token = bearerToken;
+            //NEXT middleware
+            next();
+        }else{
+        //forbidden '403'
+        res.sendStatus(403);
+        }
 
-            if(err){
-                res.send(err);
-            }else{
-                res.send(results);
-            }
-        });
-    });*/
+    }
    
 }
     //AINDA NÃO FUNCIONAL
