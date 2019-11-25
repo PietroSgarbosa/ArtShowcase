81822e3d85cf027184e6d3fa1da3f14eb7b05c90
Sky-Horse-Labs <sky.horse.development@gmail.com>
2019-11-25 11:57:30 -0300

    Create champonship.js

---
+++ routes/championship.js
@@ -0,0 +1,22 @@
+const conn = require('../dbconfig');
+
+module.exports = (app)=>{
+   
+    //RETURNS ALL CHAMPIONSHIPS AVAILABLE
+    app.get('/championship/get_championship', (req, res) =>{
+
+        let sql = `SELECT CODI_CAMPE as codi_campe, CODI_SITUA as situacao, CODI_USUAR as criador, 
+        TITU_CAMPE as titulo, DESC_CAMPE as descricao, TEMA_CAMPE as tema, DATA_CRIAC as data_criacao FROM campeonatos;`;               
+                                                                                        
+        conn.query(sql, (err, results)=>{ 
+            
+            if(err){                                        
+                res.sendStatus(400);                        
+            }else{
+                
+                res.json({status : 200, results});
+            }
+        });   
+    });
+}
+
