539d4a1fba10a22e7b70e6a8b8d169fde35a4097
Sky-Horse-Labs <sky.horse.development@gmail.com>
2019-11-25 13:49:28 -0300

    update championship

--- routes/championship.js
+++ routes/championship.js
@@ -9,9 +9,11 @@ module.exports = (app)=>{
         (SELECT A.NOME_USUAR FROM CADASTRO_USUARIO AS A, CAMPEONATOS AS B WHERE A.CODI_USUAR = B.CODI_USUAR) as criador, 
         TITU_CAMPE as titulo, DESC_CAMPE as descricao, TEMA_CAMPE as tema, DATA_CRIAC as data_criacao FROM campeonatos;`;  */  
 
-        let sql = `SELECT CODI_CAMPE as codi_campe, CODI_SITUA as situacao, 
-        CODI_USUAR as criador, 
-        TITU_CAMPE as titulo, DESC_CAMPE as descricao, TEMA_CAMPE as tema, DATA_CRIAC as data_criacao FROM campeonatos;`;               
+        let sql = `SELECT a.CODI_CAMPE as codi_campe, a.CODI_SITUA as situacao, 
+        b.NOME_USUAR as criador,
+        a.TITU_CAMPE as titulo, a.DESC_CAMPE as descricao, a.TEMA_CAMPE as tema, a.DATA_CRIAC as data_criacao 
+        FROM campeonatos a
+        join CADASTRO_USUARIO b on b.CODI_USUAR = a.CODI_USUAR;`;               
                                                                                         
         conn.query(sql, (err, results)=>{ 
             
