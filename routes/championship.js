c7875f30db6e9af664f01b0d28a1d7876fde4975
Sky-Horse-Labs <sky.horse.development@gmail.com>
2019-11-25 12:08:32 -0300

    update championship.js

--- routes/championship.js
+++ routes/championship.js
@@ -5,7 +5,8 @@ module.exports = (app)=>{
     //RETURNS ALL CHAMPIONSHIPS AVAILABLE
     app.get('/championship/get_championship', (req, res) =>{
 
-        let sql = `SELECT CODI_CAMPE as codi_campe, CODI_SITUA as situacao, CODI_USUAR as criador, 
+        let sql = `SELECT CODI_CAMPE as codi_campe, CODI_SITUA as situacao, 
+        (SELECT A.NOME_USUAR FROM CADASTRO_USUARIO AS A, CAMPEONATOS AS B WHERE A.CODI_USUAR = B.CODI_USUAR) as criador, 
         TITU_CAMPE as titulo, DESC_CAMPE as descricao, TEMA_CAMPE as tema, DATA_CRIAC as data_criacao FROM campeonatos;`;               
                                                                                         
         conn.query(sql, (err, results)=>{ 
